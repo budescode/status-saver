6b93cdcccb71b1f0381f9e880f17fd7785482035
Omonbude Emmanuel <gospeltruth18@gmail.com>
2020-12-14 11:29:29 +0100

    images loaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,9 +33,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String firstimage;
+  @override
+  void initState() {
+    super.initState();
+   
+    getFilePath();
+    super.initState();
+  }
 
+ @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+ }
+
+  String firstimage;
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+  List<FileSystemEntity> videoslist;
+  List<FileSystemEntity> imageslist;
+  bool loading = true;
   Future<void> getFilePath() async {
+    await checkPermission();
+    setState(() {
+      loading = true;
+    });
+    
     Directory appDocDir1 = await getExternalStorageDirectory();
     String appDocPath1 = appDocDir1.path;
     String path = appDocPath1.toString();
@@ -45,22 +68,39 @@ class _MyHomePageState extends State<MyHomePage> {
     var directory = new Directory(newdir);
     List<dynamic> thefile1 = directory.listSync();
     File newfile = thefile1[1];
+    var videos = thefile1.where((f) => f.path.endsWith('.mp4')).toList();
+    var jpgimage = thefile1.where((f) => f.path.endsWith('.jpg')).toList();
+    var pngimage = thefile1.where((f) => f.path.endsWith('.png')).toList();
+    var newvideo = videos[0];
+    File myfile = File(newvideo.path);
+    print(myfile);
+
     setState((){
-      firstimage = newfile.path;          
+      firstimage = newfile.path;    
+      videoslist = videos;
+      imageslist = jpgimage + pngimage;
+      _controller = VideoPlayerController.file(myfile);  
+      _initializeVideoPlayerFuture = _controller.initialize();
+      _controller.setLooping(true);
+      _controller.setVolume(1.0);
+      _controller.play();
+      loading = false;
     });
-    print(firstimage);
-  }
+
+   }
 
   checkPermission() async {
     var storageStatus = await Permission.storage.status;
-    if (storageStatus == false){
+    print(storageStatus);
+    if (storageStatus != true){
       await Permission.storage.request() ;
     }
+    print(storageStatus);
   }
 
   @override
   Widget build(BuildContext context) {
-return DefaultTabController(
+  return DefaultTabController(
   length: 2,
   child: MaterialApp(
     home: Scaffold(
@@ -84,45 +124,42 @@ return DefaultTabController(
       drawer: Drawer(),
       body: TabBarView(
         children: [
+                      loading ?CircularProgressIndicator() : GridView.count(
+  crossAxisCount: 2 ,
+  children: List.generate(imageslist.length,(index){
+    return Padding(
+      padding: const EdgeInsets.only(bottom:5.0),
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(       
+                  image: DecorationImage(
+                        image: FileImage(File(imageslist[index].path))
+           )
+      )
+        // child: Text('yayaya')
+      ),
+    );
+  }),
+),
           Center(
-            child: GestureDetector(
-            onTap: () async {
-              //getFilePath();
-              await checkPermission();
-              getFilePath();
-              
-            },
-            child: Icon(Icons.delete)),
-            ),
-          Center(
-              child: Container(
-                height:180,
-                width:100,
-                decoration: BoxDecoration(       
-                image: DecorationImage(
-                      image: FileImage(File('$firstimage'))
-         )
-    )
-)
+            //   child: AspectRatio(
+            //   aspectRatio: _controller.value.aspectRatio,
+            //   child: Stack(
+            //     alignment: Alignment.bottomCenter,
+            //     children: <Widget>[
+            //       VideoPlayer(_controller),
+            //       ClosedCaption(text: _controller.value.caption.text),
+            //       VideoProgressIndicator(_controller, allowScrubbing: true),
+            //     ],
+            //   ),
+            // ),
           ),
+          
         ],
       ),
     ),
   ),
 );
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text(widget.title),
-    //       actions: [
-    //         IconButton(icon: Icon(Icons.share, color:Colors.white), onPressed: null),
-    //         IconButton(icon: Icon(Icons.help, color:Colors.white), onPressed: null),
-            
-    //       ],
-    //     backgroundColor: Color.fromRGBO(18, 140, 126, 1),
-    //   ),
-    //   drawer: Drawer(),
-    //   body: Center(     
-    //   ),
-    // );
+
   }
 }
