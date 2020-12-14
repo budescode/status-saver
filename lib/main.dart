aeaa4393c81747cf47d0d23eb872a2a603a920f3
Timothy Brown <Boxerbuzz559@gmail.com>
2020-12-14 14:25:37 +0100

    merge
    Merged modification with upstream main branch

--- lib/main.dart
+++ lib/main.dart
@@ -33,50 +33,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-   
-    getFilePath();
-    super.initState();
-  }
-
- @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
- }
-
-  String firstimage;
+  String firstImage;
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
-  List<FileSystemEntity> videoslist;
-  List<FileSystemEntity> imageslist;
+  List<FileSystemEntity> videoList;
+  List<FileSystemEntity> imageList;
   bool loading = true;
+
   Future<void> getFilePath() async {
-    await checkPermission();
-    setState(() {
-      loading = true;
-    });
-    
     Directory appDocDir1 = await getExternalStorageDirectory();
     String appDocPath1 = appDocDir1.path;
     String path = appDocPath1.toString();
     var theIndex = path.indexOf('/Android/data/com.example.whatsappsave/files');
     var subStr = path.substring(1, theIndex);
     var newDir = '$subStr/WhatsApp Business/Media/.Statuses';
+    var directory = new Directory(newDir);
+    List<dynamic> theFile1 = directory.listSync();
+    File newFile = theFile1[1];
+    var videos = theFile1.where((f) => f.path.endsWith('.mp4')).toList();
+    var jpgImage = theFile1.where((f) => f.path.endsWith('.jpg')).toList();
+    var pngImage = theFile1.where((f) => f.path.endsWith('.png')).toList();
+    var newVideo = videos[0];
+    File myFile = File(newVideo.path);
+    print(myFile);
+
+    setState((){
+      firstImage = newFile.path;
+      videoList = videos;
+      imageList = jpgImage + pngImage;
+      _controller = VideoPlayerController.file(myFile);
+      _initializeVideoPlayerFuture = _controller.initialize();
+      _controller.setLooping(true);
+      _controller.setVolume(1.0);
+      _controller.play();
       loading = false;
     });
-
-   }
+  }
 
   checkPermission() async {
     var storageStatus = await Permission.storage.status;
-    print(storageStatus);
-    if (storageStatus != true){
-      await Permission.storage.request() ;
+    if (storageStatus == PermissionStatus.denied) {
+      await Permission.storage.request();
     }
-    print(storageStatus);
   }
+
+  @override
+  void initState() {
+    super.initState();
+    getFilePath();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: DefaultTabController(
+        length: 2,
+        initialIndex: 0,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Color.fromRGBO(18, 140, 126, 1),
+              bottom: TabBar(
+                onTap: (index) {},
+                tabs: [
+                  Tab(icon: Icon(Icons.image)),
+                  Tab(icon: Icon(Icons.video_library)),
+                ],
+              ),
+              title: Text(widget.title),
+              pinned: true,
+              floating: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.share, color: Colors.white),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.help, color: Colors.white),
+                  onPressed: null,
+                ),
+              ],
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.5 - 50,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          await checkPermission();
+                          getFilePath();
+                        },
+                        child: Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      height: 180,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: FileImage(File('$firstImage')),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
